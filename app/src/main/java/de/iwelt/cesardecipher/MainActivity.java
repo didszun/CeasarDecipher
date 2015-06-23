b488d45e627c592575e9ad83240c222da12809f1
Michael Didszun <didszun@outlook.com>
2015-06-23 15:35:14 +0200

    initial checkin

---
+++ app/src/main/java/de/iwelt/cesardecipher/MainActivity.java
@@ -0,0 +1,93 @@
+package de.iwelt.cesardecipher;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    EditText etInput;
+    TextView tvOutput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etInput = (EditText) findViewById(R.id.input);
+        tvOutput = (TextView) findViewById(R.id.output);
+
+        etInput.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+                UpdateText();
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void UpdateText() {
+        String input = etInput.getText().toString();
+        String output = "";
+
+        for (int i = 1; i <= 26; i++) {
+            output += CesarCipherDecode(input, i) + "\n";
+        }
+
+        tvOutput.setText(output);
+    }
+
+    public static String CesarCipherDecode(String enc, int offset) {
+        return CesarCipherEncode(enc, 26 - offset);
+    }
+
+    public static String CesarCipherEncode(String enc, int offset) {
+        offset = offset % 26 + 26;
+        StringBuilder encoded = new StringBuilder();
+        for (char i : enc.toCharArray()) {
+            if (Character.isLetter(i)) {
+                if (Character.isUpperCase(i)) {
+                    encoded.append((char) ('A' + (i - 'A' + offset) % 26));
+                } else {
+                    encoded.append((char) ('a' + (i - 'a' + offset) % 26));
+                }
+            } else {
+                encoded.append(i);
+            }
+        }
+        return encoded.toString();
+    }
+}
